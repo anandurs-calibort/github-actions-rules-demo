aa42181230ec5f1600f42a24cab926d30693b18b
anandurs-clibort <anandurs@calibort.com>
2026-09-07 18:22:27 +0530

    test: verify branch update rule

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'GitHub Actions CI Demo:',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
+        cardColor: Colors.red,
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
